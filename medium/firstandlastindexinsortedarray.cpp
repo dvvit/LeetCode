ecd8c1329df1b57cd10f7a538997f23b1e7e5b7a
dvvit <dvvit1555@gmail.com>
2025-06-16 09:09:31 +0530

    first and last index of sorted array

---
+++ medium/firstandlastindexinsortedarray.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findFirst(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return {first, last};
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+    vector<int> result = solution.searchRange(nums, target);
+    cout << "[" << result[0] << ", " << result[1] << "]" << endl;
+    return 0;
+}
