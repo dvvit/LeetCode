e85ff93e445d02b5d4cbdab8493546d1bbe275a5
dvvit <dvvit1555@gmail.com>
2025-06-25 22:00:57 +0530

    confirmation rate

---
+++ medium/confirmationrate.sql
@@ -0,0 +1,3 @@
+select s.user_id, round(ifnull(sum(c.action='confirmed')/count(c.action),0),2) as confirmation_rate
+from Signups s left outer join Confirmations c on s.user_id = c.user_id
+group by s.user_id
