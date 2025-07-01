df39c2ab7698d78d7ffbd64841f022ac20530555
dvvit <dvvit1555@gmail.com>
2025-07-01 14:36:16 +0530

    Primary Department for Each Employee

---
+++ easy/PrimaryDepartmentforEachEmployee.sql
@@ -0,0 +1,6 @@
+select distinct employee_id, department_id
+from Employee 
+where employee_id in(
+    select employee_id from Employee group by employee_id having count(*) = 1
+) or primary_flag = 'Y'
+order by employee_id
