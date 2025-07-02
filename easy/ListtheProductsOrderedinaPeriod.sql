d73c263077d6e5cf87319f01cd967803fba8c87b
dvvit <dvvit1555@gmail.com>
2025-07-02 20:14:39 +0530

    List the Products Ordered in a Period

---
+++ easy/ListtheProductsOrderedinaPeriod.sql
@@ -0,0 +1,5 @@
+select product_name, sum(unit) as unit
+from Products inner join Orders using(product_id)
+where month(order_date) = 2 and year(order_date) = 2020
+group by product_name
+having unit>=100
