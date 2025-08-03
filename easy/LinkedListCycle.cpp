2959d88b667b5d7381f1f8560a6390451e25d092
dvvit <dvvit1555@gmail.com>
2025-08-03 15:47:58 +0530

    Linked List Cycle

---
+++ easy/LinkedListCycle.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr)
+            return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = head->next->next;
+
+    Solution sol;
+    cout << (sol.hasCycle(head) ? "Cycle detected" : "No cycle detected") << endl;
+
+    return 0;
+}
